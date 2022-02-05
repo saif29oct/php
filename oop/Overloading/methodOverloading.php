260bd490168a5cab3e9d5e18f9635d4857204a1e
saif29oct <saif29oct@gmail.com>
2022-02-05 11:43:47 +0600

    Polymorphism: 	Method Overloading: __call($name, $args)

--- oop/Overloading/methodOverloading.php
+++ oop/Overloading/methodOverloading.php
@@ -1,6 +1,7 @@
 <?php
 /**
  * Overloading in PHP provides means to dynamically create properties and methods. These dynamic entities are processed via magic methods one can establish in a class for various action types.
+ * 
  * The overloading methods are invoked when interacting with properties or methods that have not been declared or are not visible in the current scope. The rest of this section will use the terms inaccessible properties and inaccessible methods to refer to this combination of declaration and visibility.
  * 
  * All overloading methods must be defined as public.
@@ -8,8 +9,40 @@
  * Note: 
  *      None of the arguments of these magic methods can be passed by reference.
  * 
- * 
  * Note: 
  *      PHP's interpretation of overloading is different than most object-oriented languages. Overloading traditionally provides the ability to have multiple methods with the same name but different quantities and types of arguments.
+ * 
+ * In PHP we use __call($fn, $arguments ) magic method for function overloading.
 
  */
+
+ class Demo{
+    //  function add ($a, $b){
+    //      echo "The addition is: " .$a + $b;
+    //  }
+
+     function __call($name, $arg){
+        // Check the function name
+        // Count the arguments
+        // Design the depending on the arguments
+        if($name == "add"){
+            $c = count($arg);
+            switch($c){
+                case 2:
+                    echo $arg[0] + $arg[1];
+                    echo PHP_EOL;
+                    break;
+                case 3:
+                    echo $arg[0] + $arg[1] + $arg[2];
+                    echo PHP_EOL;
+                    break;
+                default:
+                    break;
+            }
+        }
+          
+     }
+ }
+
+ $obj1 = new Demo();
+ $obj1->add(1, 2, 3);
