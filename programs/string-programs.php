b64537e97cfaecef836fbe580b65a80e862af570
Saiful Islam <saif29oct@gmail.com>
2023-05-11 16:41:06 +0600

    program list updated.

---
+++ programs/string-programs.php
@@ -0,0 +1,34 @@
+<?php
+// 1) How to Print duplicate characters from a String?
+// 2) How to check if two Strings are anagrams of each other?
+/**
+ * A simple coding problem based upon String, but could also be asked with numbers. You need to write a Java program to check if two given strings are anagrams of Each other. Two strings are anagrams if they are written using the same exact letters, ignoring space, punctuation, and capitalization. Each letter should have the same count in both strings. For example, the Army and Mary are an anagram of each other.
+ */
+// 3) How to program to print first non-repeated character from String?
+// 4) How to reverse a String?
+// 5) How to check if a String contains only digits?
+// 6) How to find duplicate characters in a String?
+// 7) How to count a number of vowels and consonants in a String?
+// 8) How to count the occurrence of a given character in String?
+// 9) How to convert numeric String to an int?
+// 10) How to replace each given character to other e.g. blank with %20?
+// 11) How to find all permutations of String?
+// 12) How to reverse words in a sentence without using a library method?
+// 13) How to check if String is Palindrome?
+// 14) How to remove duplicate characters from String?
+// 15) How to check if a String is a valid shuffle of two String?
+// 16) Write a program to check if a String contains another String ?
+/**
+ * You need to write a function to search for the existence of a string (target) in another string (source). The function takes two strings as the input and returns the index where the second string is found. If the target string cannot be found, then return -1.
+ */
+// 17) How to return the highest occurred character in a String?
+// 18) Write a program to remove a given character from String?
+// 19) Write a program to find the longest palindrome in a string?
+// 20) How to sort String on their length?
+/**
+ * Your method should accept an array of String and return a sorted array based upon the length of String.
+ */
+// 21) How to find the length of the longest substring with K distinct characters?
+// Ref: https://www.sanfoundry.com/c-programming-examples-arrays/
+// Ref: https://www.geeksforgeeks.org/top-50-array-coding-problems-for-interviews/
+
