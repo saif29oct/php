961ed66fec60f34200d48fe8e6ec0cb09d440fd0
Saiful <saif29oct@gmail.com>
2023-05-22 07:53:28 +0600

    max min done

--- programs/MaxMin.php
+++ programs/MaxMin.php
@@ -1,15 +1,58 @@
 <?php
-// Find 2nd min value and 2nd max value from an given array;
 
-$ary = [9 ,5, 7, 15, 2, 13, 4];
-$loopLimit = count($ary)-1;
-$min2nd = 0;
-$max2nd = 0;
-for($i=0; $i<2; $i++){
-    for($j=$i; $j<$loopLimit; $j++){
-        $min2nd = min($ary[$j], $ary[$j+1]);
-        $max2nd = max($ary[$j], $ary[$j+1]);
+// Find kth highest and lowest element of an array
+$ary = [5, 7, 3, 2, 8, 6];
+/**
+ * 3rd highest element of $ary is 6
+ * 3rd lowest element of $ary is 5
+ */
+
+//---------------  Method-1  ---------------------
+$aryLength = count($ary);
+$loopCount = $aryLength - 1;
+
+$kthPosition = 3;
+
+for ($i = 0; $i < $kthPosition; $i++) {
+    for ($j = $i; $j < $loopCount; $j++) {
+        if ($ary[$i] < $ary[$j + 1]) {
+            $temp = $ary[$i];
+            $ary[$i] = $ary[$j + 1];
+            $ary[$j + 1] = $temp;
+        }
     }
 }
 
-echo "min:$min2nd,  max:$max2nd";
+echo "Highest Value of kth position: " . $ary[$kthPosition - 1];
+
+
+
+// -------------------------------------   Mehod-2   -------------------------------
+$descAry = $ary;
+$ascAry = $ary;
+$kthPosition = 3;
+$maxPosition = 0;
+$minPosition = 0;
+for ($i = 0; $i < $kthPosition; $i++) {
+    $max = 0;
+    $min = INF;
+    for ($j = 0; $j < count($descAry); $j++) {
+        if ($descAry[$j] > $max) {
+            $max = $descAry[$j];
+            $maxPosition = $j;
+        }
+        
+        if ($ascAry[$j] < $min) {
+            $min = $ascAry[$j];
+            $minPosition = $j;
+        }
+    }
+    unset($descAry[$maxPosition]);
+    $descAry = array_values($descAry);
+    
+    unset($ascAry[$minPosition]);
+    $ascAry = array_values($ascAry);
+    
+    echo "<pre>" . "max: " . $max . "  min: " . $min . "<pre>";
+}
+
